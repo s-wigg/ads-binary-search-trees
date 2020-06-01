82c9512cf158e8285e9f339fe3648a12859d603e
Severin Wiggenhorn <severin@convoy.com>
2020-06-01 01:25:02 -0700

    implement insert and delete and add tests

--- src/data_structures/binary_search_tree.js
+++ src/data_structures/binary_search_tree.js
@@ -16,7 +16,45 @@ class BinarySearchTree {
   }
 
   insert(key, value = true) {
-    // TODO
+    let node = this._root;
+    let prev = node;
+    if (!node) {
+      this._root = new BSTNode({
+        key,
+        value,
+        parent: null,
+        left: null,
+        right: null,
+      });
+      this._count++;
+      return value;
+    }
+
+    while (node) {
+      if (key < node.key) {
+        prev = node;
+        node = node.left;
+      } else if (key > node.key) {
+        prev = node;
+        node = node.right;
+      } else {
+        // we wont insert duplicate keys, so just update value
+        return (node.value = value);
+      }
+    }
+    const newNode = new BSTNode({
+      key,
+      value,
+      parent: prev,
+      left: null,
+      right: null,
+    });
+    this._count++;
+    if (key > prev.key) {
+      prev.right = newNode;
+    } else {
+      prev.left = newNode;
+    }
   }
 
   lookup(key) {
@@ -27,14 +65,80 @@ class BinarySearchTree {
         node = node.left;
       } else if (key > node.key) {
         node = node.right;
-      } else { // equal
+      } else {
+        // equal
         return node.value;
       }
     }
   }
 
+  _updateParentNode(parentNode, key, replacement) {
+    if (parentNode) {
+      if (parentNode?.left?.key === key) {
+        parentNode.left = replacement;
+      } else {
+        parentNode.right = replacement;
+      }
+    } else {
+      return null;
+    }
+  }
+
+  _deleteNode(node, key) {
+    if (!node) {
+      return null;
+    }
+
+    if (key < node.key) {
+      node.left = this._deleteNode(node.left, key);
+      return node;
+    } else if (key > node.key) {
+      node.right = this._deleteNode(node.right, key);
+      return node;
+    } else {
+      // key matches, this is the node to delete
+      this._count--;
+      if (!node.left && !node.right) {
+        // no children
+        node.parent = this._updateParentNode(node.parent, key, null);
+        node = null;
+        return node;
+      } else if (!node.left) {
+        // left child only
+        node.parent = this._updateParentNode(node.parent, key, node.right);
+        node = node.right;
+        return node;
+      } else if (!node.right) {
+        // right child only
+        node.parent = this._updateParentNode(node.parent, key, node.left);
+        node = node.left;
+        return node;
+      } else {
+        // 2 children
+        let smallestNode = this._findSmallestValueNode(node.right);
+        node.key = smallestNode.key;
+        node.value = smallestNode.value;
+        // hacky but increment the count because we're going to re-delete with the recursive call below
+        this._count++;
+        node.right = this._deleteNode(node.right, smallestNode.key);
+        return node;
+      }
+    }
+  }
+
   delete(key) {
-    // TODO (tests first!)
+    let node = this._root;
+    let deletedNode = this.lookup(key);
+    const returnedFromDelete = this._deleteNode(node, key);
+    this._root = returnedFromDelete;
+    return deletedNode;
+  }
+
+  _findSmallestValueNode(node) {
+    while (node.left) {
+      node = node.left;
+    }
+    return node;
   }
 
   count() {
@@ -53,8 +157,8 @@ class BinarySearchTree {
         i = visitSubtree(node.right, callback, i + 1);
       }
       return i;
-    }
-    visitSubtree(this._root, callback)
+    };
+    visitSubtree(this._root, callback);
   }
 }
 
--- src/data_structures/binary_search_tree.test.js
+++ src/data_structures/binary_search_tree.test.js
@@ -1,62 +1,62 @@
-import BinarySearchTree from './binary_search_tree';
+import BinarySearchTree from "./binary_search_tree";
 
 const dataStructures = [
   BinarySearchTree,
   // We'll add more next week
 ];
 
-dataStructures.forEach(TargetDS => {
+dataStructures.forEach((TargetDS) => {
   describe(TargetDS, () => {
     let bst;
     beforeEach(() => {
       bst = new TargetDS();
     });
 
-    it('starts empty', () => {
+    it("starts empty", () => {
       expect(bst.count()).toBe(0);
     });
 
-    describe('lookup', () => {
-      it('returns undefined on an empty tree', () => {
-        expect(bst.lookup('test')).toBe(undefined);
+    describe("lookup", () => {
+      it("returns undefined on an empty tree", () => {
+        expect(bst.lookup("test")).toBe(undefined);
       });
 
-      it('returns undefined if the key is not in the tree', () => {
-        const keys = ['many', 'keys', 'for', 'this', 'tree'];
+      it("returns undefined if the key is not in the tree", () => {
+        const keys = ["many", "keys", "for", "this", "tree"];
         keys.forEach((key, i) => {
           bst.insert(key);
         });
 
-        expect(bst.lookup('dne')).toBe(undefined);
+        expect(bst.lookup("dne")).toBe(undefined);
       });
 
-      it('finds the only record', () => {
-        bst.insert('test');
-        expect(bst.lookup('test')).toBeTruthy();
+      it("finds the only record", () => {
+        bst.insert("test");
+        expect(bst.lookup("test")).toBeTruthy();
       });
 
-      it('finds any extant record', () => {
-        const keys = ['many', 'keys', 'for', 'this', 'tree'];
-        keys.forEach(key => {
+      it("finds any extant record", () => {
+        const keys = ["many", "keys", "for", "this", "tree"];
+        keys.forEach((key) => {
           bst.insert(key);
         });
 
-        keys.forEach(key => {
+        keys.forEach((key) => {
           expect(bst.lookup(key)).toBeTruthy();
         });
 
-        keys.reverse().forEach(key => {
+        keys.reverse().forEach((key) => {
           expect(bst.lookup(key)).toBeTruthy();
         });
       });
 
-      it('returns the value associated with a record', () => {
+      it("returns the value associated with a record", () => {
         const records = [
-          { key: 'one', value: 'first' },
-          { key: 'two', value: 'second' },
-          { key: 'three', value: 'third' },
-          { key: 'four', value: 'fourth' },
-          { key: 'five', value: 'fifth' },
+          { key: "one", value: "first" },
+          { key: "two", value: "second" },
+          { key: "three", value: "third" },
+          { key: "four", value: "fourth" },
+          { key: "five", value: "fifth" },
         ];
 
         records.forEach(({ key, value }) => {
@@ -73,141 +73,304 @@ dataStructures.forEach(TargetDS => {
       });
     });
 
-    describe('insert', () => {
-      it('increases count by 1', () => {
+    describe("insert", () => {
+      it("increases count by 1", () => {
         expect(bst.count()).toBe(0);
-        bst.insert('test');
+        bst.insert("test");
         expect(bst.count()).toBe(1);
 
-        const keys = ['many', 'keys', 'for', 'this', 'tree'];
+        const keys = ["many", "keys", "for", "this", "tree"];
         keys.forEach((key, i) => {
           bst.insert(key);
           expect(bst.count()).toBe(2 + i);
         });
       });
 
-      it('replaces records with the same key and does not increase the count', () => {
-        bst.insert('test', 'first value');
+      it("replaces records with the same key and does not increase the count", () => {
+        bst.insert("test", "first value");
         expect(bst.count()).toBe(1);
-        expect(bst.lookup('test')).toBe('first value');
+        expect(bst.lookup("test")).toBe("first value");
 
-        bst.insert('test', 'second value');
+        bst.insert("test", "second value");
         expect(bst.count()).toBe(1);
-        expect(bst.lookup('test')).toBe('second value');
+        expect(bst.lookup("test")).toBe("second value");
       });
 
-      it('uses true as the default value', () => {
-        bst.insert('test');
-        expect(bst.lookup('test')).toBe(true);
+      it("uses true as the default value", () => {
+        bst.insert("test");
+        expect(bst.lookup("test")).toBe(true);
       });
     });
 
-    describe('delete', () => {
-      it('returns the value for the removed record', () => {
-
-      });
-
-      it('returns undefined if the record was not found', () => {
-
-      });
-
-      it('reduces the count by 1', () => {
-
-      });
-
-      it('omits the removed record from iteration results', () => {
-
-      });
-
-      it('can remove every element in a tree', () => {
-
-      });
-
-      describe('scenarios', () => {
-        // The first step for each of these tests will be to construct
-        // a tree matching the scenario. How can you use your knowledge
-        // of how insert works to do this? How can you check your work?
-
-        it('can remove the record with the smallest key', () => {
-          // TODO:
-          // Insert several records
-          // Remove the record with the smallest key
-          // Ensure that looking up that key returns undefined
-        });
-
-        it('can remove the record with the largest key', () => {
-
-        });
-
-        it('can remove the root', () => {
-
-        });
-
-        it('can remove a node with no children', () => {
-
-        });
-
-        it('can remove a node with only a left child', () => {
-
-        });
-
-        it('can remove a node with only a right child', () => {
-
-        });
-
-        it('can remove a node with both children, where the successor is the node\'s right child', () => {
-
-        });
-
-        it('can remove a node with both children, where the successor is not the node\'s right child', () => {
-
-        });
-      });
-    });
-
-    describe('forEach', () => {
+    describe("delete", () => {
       let records;
       beforeEach(() => {
         records = [
-          { key: 'one', value: 'first' },
-          { key: 'two', value: 'second' },
-          { key: 'three', value: 'third' },
-          { key: 'four', value: 'fourth' },
-          { key: 'five', value: 'fifth' },
+          { key: "one", value: "first" },
+          { key: "two", value: "second" },
+          { key: "three", value: "third" },
+          { key: "four", value: "fourth" },
+          { key: "five", value: "fifth" },
         ];
       });
-
-      const sortRecords = (records) => {
-        return records.sort((a, b) => a.key.localeCompare(b.key));
-      }
 
       const fill = (records) => {
         records.forEach(({ key, value }) => {
           bst.insert(key, value);
         });
-      }
+      };
 
-      it('runs the callback 0 times on an empty tree', () => {
+      it("returns the value for the removed record", () => {
+        records.forEach(({ key, value }) => {
+          bst.insert(key, value);
+        });
+
+        expect(bst.delete("five")).toBe("fifth");
+      });
+
+      it("returns undefined if the record was not found", () => {
+        records.forEach(({ key, value }) => {
+          bst.insert(key, value);
+        });
+
+        expect(bst.delete("fifty")).toBe(undefined);
+      });
+
+      it("reduces the count by 1", () => {
+        records.forEach(({ key, value }) => {
+          bst.insert(key, value);
+        });
+
+        bst.delete("two");
+
+        expect(bst.count()).toBe(4);
+      });
+
+      it("omits the removed record from iteration results", () => {
+        fill(records);
+
+        bst.delete("five");
+        bst.delete("two");
+
+        bst.forEach(({ key, _value }) => {
+          expect(key).not.toBe("two");
+          expect(key).not.toBe("five");
+        });
+        expect(bst.count()).toBe(3);
+      });
+
+      it("can remove every element in a tree", () => {
+        const records = [
+          { key: "one", value: "first" },
+          { key: "two", value: "second" },
+        ];
+
+        records.forEach(({ key, value }) => {
+          bst.insert(key, value);
+        });
+
+        bst.delete("one");
+        bst.delete("two");
+        expect(bst.count()).toBe(0);
+      });
+
+      describe("scenarios", () => {
+        it("can remove the record with the smallest key", () => {
+          const records = [
+            { key: 100, value: "first" },
+            { key: 500, value: "second" },
+            { key: 5, value: "third" },
+            { key: 555, value: "fourth" },
+          ];
+          fill(records);
+          bst.delete(5);
+
+          expect(bst.lookup(5)).toBe(undefined);
+          const keys = [100, 500, 555];
+
+          let i = 0;
+          bst.forEach(({ key, _value }) => {
+            expect(key).not.toBe("5");
+            expect(keys[i]).toBe(key);
+            i++;
+          });
+
+          expect(bst.count()).toBe(3);
+        });
+
+        it("can remove the record with the largest key", () => {
+          const records = [
+            { key: 100, value: "first" },
+            { key: 500, value: "second" },
+            { key: 5, value: "third" },
+            { key: 555, value: "fourth" },
+          ];
+          fill(records);
+          bst.delete(555);
+
+          expect(bst.lookup(555)).toBe(undefined);
+
+          const keys = [5, 100, 500];
+          let i = 0;
+
+          bst.forEach(({ key, _value }) => {
+            expect(key).not.toBe(555);
+            expect(keys[i]).toBe(key);
+
+            i++;
+          });
+          expect(bst.count()).toBe(3);
+        });
+
+        it("can remove the root", () => {
+          const records = [{ key: "one", value: "first" }];
+          fill(records);
+          bst.delete("one");
+
+          expect(bst.lookup("one")).toBe(undefined);
+          bst.forEach(({ key, _value }) => {
+            expect(key).not.toBe("one");
+          });
+          expect(bst.count()).toBe(0);
+        });
+
+        it("can remove a node with no children", () => {
+          const records = [
+            { key: "100", value: "first" },
+            { key: "500", value: "second" },
+          ];
+          fill(records);
+          bst.delete("500");
+
+          expect(bst.lookup("500")).toBe(undefined);
+          bst.forEach(({ key, _value }) => {
+            expect(key).not.toBe("by");
+          });
+          expect(bst.count()).toBe(1);
+        });
+
+        it("can remove a node with only a left child", () => {
+          const records = [
+            { key: 10, value: "first" },
+            { key: 5, value: "second" },
+            { key: 2, value: "third" },
+            { key: 15, value: "fourth" },
+          ];
+          fill(records);
+          bst.delete(2);
+
+          expect(bst.lookup(2)).toBe(undefined);
+
+          const keys = [5, 10, 15];
+          let i = 0;
+
+          bst.forEach(({ key, _value }) => {
+            expect(key).not.toBe(2);
+            expect(keys[i]).toBe(key);
+            i++;
+          });
+          expect(bst.count()).toBe(3);
+        });
+
+        it("can remove a node with only a right child", () => {
+          const records = [
+            { key: 10, value: "first" },
+            { key: 5, value: "second" },
+            { key: 15, value: "third" },
+            { key: 20, value: "fourth" },
+          ];
+          fill(records);
+          bst.delete(15);
+
+          expect(bst.lookup(15)).toBe(undefined);
+
+          const keys = [5, 10, 20];
+          let i = 0;
+
+          bst.forEach(({ key, _value }) => {
+            expect(key).not.toBe(2);
+            expect(keys[i]).toBe(key);
+            i++;
+          });
+          expect(bst.count()).toBe(3);
+        });
+
+        it("can remove a node with both children, where the successor is the node's right child", () => {
+        });
+
+        it("can remove a node with both children, where the successor is not the node's right child", () => {
+          const records = [
+            { key: 10, value: "first" },
+            { key: 5, value: "second" },
+            { key: 15, value: "third" },
+            { key: 20, value: "fourth" },
+            { key: 13, value: "fifth" },
+          ];
+          fill(records);
+          expect(bst.count()).toBe(5);
+
+          bst.delete(15);
+          expect(bst.count()).toBe(4);
+
+          expect(bst.lookup(15)).toBe(undefined);
+
+          const keys = [5, 10, 13, 20];
+          let i = 0;
+
+          let test = [];
+          bst.forEach(({ key, _value }) => {
+            test.push(key);
+            expect(key).not.toBe(15);
+            expect(keys[i]).toBe(key);
+            i++;
+          });
+        });
+      });
+    });
+
+    describe("forEach", () => {
+      let records;
+      beforeEach(() => {
+        records = [
+          { key: "one", value: "first" },
+          { key: "two", value: "second" },
+          { key: "three", value: "third" },
+          { key: "four", value: "fourth" },
+          { key: "five", value: "fifth" },
+        ];
+      });
+
+      const sortRecords = (records) => {
+        return records.sort((a, b) => a.key.localeCompare(b.key));
+      };
+
+      const fill = (records) => {
+        records.forEach(({ key, value }) => {
+          bst.insert(key, value);
+        });
+      };
+
+      it("runs the callback 0 times on an empty tree", () => {
         const cb = jest.fn();
         bst.forEach(cb);
 
         expect(cb.mock.calls.length).toBe(0);
       });
 
-      it('provides {key, value}, index and tree as cb args', () => {
-        bst.insert('key', 'value');
+      it("provides {key, value}, index and tree as cb args", () => {
+        bst.insert("key", "value");
 
         const cb = jest.fn();
         bst.forEach(cb);
 
         const callArgs = cb.mock.calls[0];
-        expect(callArgs[0].key).toBe('key');
-        expect(callArgs[0].value).toBe('value');
+        expect(callArgs[0].key).toBe("key");
+        expect(callArgs[0].value).toBe("value");
         expect(callArgs[1]).toBe(0);
         expect(callArgs[2]).toBe(bst);
       });
 
-      it('iterates records in key order', () => {
+      it("iterates records in key order", () => {
         fill(records);
 
         const cb = jest.fn();
@@ -222,7 +385,7 @@ dataStructures.forEach(TargetDS => {
         });
       });
 
-      it('iterates correctly for sorted input', () => {
+      it("iterates correctly for sorted input", () => {
         fill(sortRecords(records));
 
         const cb = jest.fn();
@@ -237,7 +400,7 @@ dataStructures.forEach(TargetDS => {
         });
       });
 
-      it('iterates correctly for reverse-sorted input', () => {
+      it("iterates correctly for reverse-sorted input", () => {
         fill(sortRecords(records).reverse());
 
         const cb = jest.fn();
